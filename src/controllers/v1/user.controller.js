82f3eb72b0a620fbe76d84280b4f3729239c91dd
taeyong_ng <taeyong.lee@ngenius.co.kr>
2019-11-27 15:14:48 +0900

    uuid 조회 및 test 구현

--- src/controllers/v1/user.controller.js
+++ src/controllers/v1/user.controller.js
@@ -1,11 +1,30 @@
-import { models } from '../../models'
+import httpStatus from 'http-status'
+import createError from 'http-errors'
+import {
+    models
+} from '../../models'
 
 const get = async (req, res, next) => {
     try {
-        const users = await models.User.findAll();
+        if (req.params.uuid) {
+            const user = await models.User.findOne({
+                where: {
+                    uuid: Buffer(req.params.uuid, 'hex')
+                }
+            })
 
-        return res.json(users)
+            if (!user) {
+                throw (createError(httpStatus.NOT_FOUND, '사용자를 찾을 수 없습니다.'))
+            }
 
+            return res
+                .status(httpStatus.OK)
+                .json(user)
+        } else {
+            const users = await models.User.findAll()
+
+            return res.json(users)
+        }
     } catch (e) {
         next(e)
     }
